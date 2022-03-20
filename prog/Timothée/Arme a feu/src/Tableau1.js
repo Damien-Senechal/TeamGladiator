99bc35c57c9373c5e17c8af3a765a74fa9d4771c
Kancabricot <92451452+Kancabricot@users.noreply.github.com>
2022-03-20 19:19:24 +0100

    Ajout de la balle et du boss et perso deplacement

--- prog/Timothée/Arme a feu/src/Tableau1.js
+++ prog/Timothée/Arme a feu/src/Tableau1.js
@@ -3,12 +3,12 @@ class Tableau1 extends Phaser.Scene {
 
     preload() {
         // Je preload les images autres que Tiled
-        this.load.image('circle','assets/circle.png');
-        this.load.image('circleG','assets/circleG.png');
-        this.load.image('circleB','assets/circleB.png');
 
-        this.load.image('Arme1','assets/square.png');
-        this.load.image('Arme2','assets/squareY.png');
+        this.load.image('circleG','assets/circleG.png');
+
+
+        this.load.image('balle','assets/square.png');
+        this.load.image('boss','assets/squareY.png');
 
         // chargement tilemap
         this.load.image("tilemap", "assets/tiles_packed.png");
@@ -20,35 +20,24 @@ class Tableau1 extends Phaser.Scene {
 
     create() {
 
-        // Création du personnage de base
-        this.perso = this.physics.add.sprite(500, 0, 'circle').setOrigin(0, 0);
-        this.perso.setDisplaySize(30,30);
-        this.perso.body.setAllowGravity(true);
-        this.perso.setVisible(true);
-
         // Création du personnage armé
         this.persoA = this.physics.add.sprite(500, 0, 'circleG').setOrigin(0, 0);
         this.persoA.setDisplaySize(30,30);
         this.persoA.body.setAllowGravity(true);
-        this.persoA.setVisible(false);
+        this.persoA.setVisible(true);
 
-        // Création du personnage avec une deuxieme arme
-        this.persoA2 = this.physics.add.sprite(500, 0, 'circleB').setOrigin(0, 0);
-        this.persoA2.setDisplaySize(30,30);
-        this.persoA2.body.setAllowGravity(true);
-        this.persoA2.setVisible(false);
 
-        // Création de l'arme qui sera au sol
-        this.arme = this.physics.add.sprite(150, 0,'Arme1').setOrigin(0, 0);
-        this.arme.setDisplaySize(15,15);
-        this.arme.body.setAllowGravity(true);
-        this.arme.setImmovable(true);
+        //Balle
+        this.Mballe = new Balle(this)
 
-        // Création de l'arme qui sera au sol
-        this.arme2 = this.physics.add.sprite(200, 0,'Arme2').setOrigin(0, 0);
-        this.arme2.setDisplaySize(15,15);
-        this.arme2.body.setAllowGravity(true);
-        this.arme2.setImmovable(true);
+
+        // Création d'un boss a tuer
+        this.boss = this.physics.add.sprite(150, 0,'balle').setOrigin(0, 0);
+        this.boss.setDisplaySize(50,50);
+        this.boss.body.setAllowGravity(true);
+        this.boss.setImmovable(true);
+
+
 
 
 
@@ -70,14 +59,11 @@ class Tableau1 extends Phaser.Scene {
 
         // Creation des collision
 
-        this.physics.add.collider(this.perso, platforms);
+
         this.physics.add.collider(this.persoA, platforms);
-        this.physics.add.collider(this.persoA2, platforms);
 
-        this.physics.add.collider(this.arme, platforms);
-        this.physics.add.collider(this.arme2, platforms);
+        this.physics.add.collider(this.boss, platforms);
 
-        this.initKeyboard();
     }
 
     // fonction pour faire regarder s'il y a un overlaps donc deux objets qui se touche pour l'utilisé plus facilement.
@@ -91,6 +77,18 @@ class Tableau1 extends Phaser.Scene {
         }
     }
 
+    tir(){
+        console.log("pan pan")
+    }
+
+    balleAucentre(){
+        this.Mballe.balle.x = this.largeur/2
+        this.Mballe.balle.y = this.hauteur/2
+        this.Mballe.balle.setVelocityX(0)
+        this.Mballe.balle.setVelocityY(0)
+        this.Mballe.balle.setVelocityX(Math.random()>0.5?-200:200)
+    }
+
     initKeyboard() {
         let me = this;
 
@@ -99,16 +97,15 @@ class Tableau1 extends Phaser.Scene {
 
                 case Phaser.Input.Keyboard.KeyCodes.Q:
 
-                    me.perso.setVelocityX(0);
                     me.persoA.setVelocityX(0);
-                    me.persoA2.setVelocityX(0);
+
                     break;
 
                 case Phaser.Input.Keyboard.KeyCodes.D:
 
-                    me.perso.setVelocityX(0);
+
                     me.persoA.setVelocityX(0);
-                    me.persoA2.setVelocityX(0);
+
                     break;
             }
         })
@@ -117,71 +114,30 @@ class Tableau1 extends Phaser.Scene {
 
                 case Phaser.Input.Keyboard.KeyCodes.Q:
 
-
-                        me.perso.setVelocityX(-300);
                         me.persoA.setVelocityX(-300);
-                        me.persoA2.setVelocityX(-300);
 
                     break;
 
                 case Phaser.Input.Keyboard.KeyCodes.D:
 
-                        me.perso.setVelocityX(300);
                         me.persoA.setVelocityX(300);
-                        me.persoA2.setVelocityX(300);
 
                     break;
 
-                case Phaser.Input.Keyboard.KeyCodes.E:
+                    // les tires dans chaques directions
 
-                        if (me.checkCollider(me.perso.x,me.perso.y,20,30,me.arme.x,me.arme.y,15,15)
-                            ===
-                            true) {
-                           me.arme.setVisible(false);
-                           me.perso.setVisible(false);
-                           me.persoA2.setVisible(false);
+                case Phaser.Input.Keyboard.KeyCodes.NUMPAD_FOUR:
 
-                           // Pour être sur que le Personnage Armé soit au bonne endroit on lui met les bonne coordonné au cas où
-                            me.persoA.x = me.perso.x;
-                            me.persoA.y = me.perso.y;
-                           me.persoA.setVisible(true);
-                        }
+                    tir();
 
-                        if (me.checkCollider(me.perso.x,me.perso.y,20,30,me.arme2.x,me.arme2.y,15,15)
-                            ===
-                            true) {
-                            me.arme2.setVisible(false);
-                            me.perso.setVisible(false);
-                            me.persoA.setVisible(false);
-
-                            // Pour être sur que le Personnage Armé soit au bonne endroit on lui met les bonne coordonné au cas où
-                            me.persoA2.x = me.perso.x;
-                            me.persoA2.y = me.perso.y;
-                            me.persoA2.setVisible(true);
-                        }
-                        break;
-
-                        // une action qui pose l'arme que on as en main.
-
-                case Phaser.Input.Keyboard.KeyCodes.A:
-
-                    if (me.persoA.visible === true) {
-                        me.persoA.setVisible(false);
-
-                        // Pour être sur que le Personnage et l'arme soit au bonne endroit on lui met les bonne coordonné au cas où
-                        me.perso.x = me.persoA.x
-                        me.perso.y = me.persoA.y
-                        me.perso.setVisible(true)
-
-                        me.arme.x = me.persoA.x
-                        me.arme.y = me.persoA.y
-                        me.arme.setVisible(true)
-
-
-                    }
                     break;
             }
         })
+    }
+
+    update(){
+
+        this.initKeyboard();
     }
 
 
