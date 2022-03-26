b4649c716dc680b38de8605fbbdfb0a83ad6af38
Kancabricot <92451452+Kancabricot@users.noreply.github.com>
2022-03-26 14:02:40 +0100

    Ajout d'une deuxieme armes

--- prog/Timothée/Arme a feu/src/Tableau1.js
+++ prog/Timothée/Arme a feu/src/Tableau1.js
@@ -32,7 +32,7 @@ class Tableau1 extends Phaser.Scene {
         this.persoA.setVisible(true);
 
 
-        // Création d'un boss a tuer
+        // Création d'une cible à tuer
         this.boss = this.physics.add.sprite(150, 0,'boss').setOrigin(0, 0);
         this.boss.setDisplaySize(50,50);
         this.boss.body.setAllowGravity(true);
@@ -80,12 +80,13 @@ class Tableau1 extends Phaser.Scene {
 
     checkCollider(Objet1x,Objet1y,Object1TailleLargeur,Object1TailleHauteur,Objet2x,Objet2y,Objet2TaileLargeur,Objet2TailleHauteur){
         if (Objet1x + Object1TailleLargeur > Objet2x && Objet1x < Objet2x + Objet2TaileLargeur
-                                            &&
+            &&
             Objet1y + Object1TailleHauteur > Objet2y && Objet1y < Objet2y + Objet2TailleHauteur) {
             // Si toutes les conditons sont vrais alors il y a bien un overlaps, on renvoie donc true/vrai a notre foncion sinon on ne renvoie rien
             return true
         }
     }
+
 
     tir(){let me = this;
         this.chargeur -= 1;
@@ -128,7 +129,7 @@ class Tableau1 extends Phaser.Scene {
 
                 case Phaser.Input.Keyboard.KeyCodes.Q:
 
-                        me.persoA.setVelocityX(-300);
+                    me.persoA.setVelocityX(-300);
 
                     break;
 
@@ -148,7 +149,7 @@ class Tableau1 extends Phaser.Scene {
 
                     break;
 
-                    // les tires dans chaque directions
+                // les tires dans chaque directions
 
                 case Phaser.Input.Keyboard.KeyCodes.NUMPAD_FOUR:
 
@@ -187,7 +188,7 @@ class Tableau1 extends Phaser.Scene {
                         me.dirx = -250
                         me.tir();
 
-                    console.log(me.chargeur)
+                        console.log(me.chargeur)
                     }
 
                     break;
@@ -220,7 +221,7 @@ class Tableau1 extends Phaser.Scene {
                     }
 
 
-                        break;
+                    break;
                 case Phaser.Input.Keyboard.KeyCodes.NUMPAD_THREE:
 
                     if(me.chargeur === 0){
@@ -234,7 +235,7 @@ class Tableau1 extends Phaser.Scene {
                     }
 
 
-                        break;
+                    break;
                 case Phaser.Input.Keyboard.KeyCodes.NUMPAD_TWO:
 
                     if(me.chargeur === 0){
@@ -248,7 +249,7 @@ class Tableau1 extends Phaser.Scene {
                     }
 
 
-                        break;
+                    break;
                 case Phaser.Input.Keyboard.KeyCodes.NUMPAD_ONE:
 
                     if(me.chargeur === 0){
@@ -262,7 +263,7 @@ class Tableau1 extends Phaser.Scene {
                     }
 
 
-                        break;
+                    break;
 
                 case Phaser.Input.Keyboard.KeyCodes.R:
 
